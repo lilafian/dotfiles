83d9609e85a89ca0a18b197bd32d70e02ad8f60d
lilaf <lilafian74@proton.me>
2025-07-05 15:14:42 -0500

    add drvses stuff

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -6,3 +6,12 @@ if ! [ -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 cp -v $REPO_DIR/dots/.zshrc ~
+
+echo "Checking for DrvSes..."
+if [ -f "/usr/bin/drvses" ]; then
+    echo "DrvSes found! Installing integration to default lilafDots theme..."
+    update_zsh_drvses_integration
+    echo "Installed DrvSes-Zsh integration!"
+fi
+
+echo "Installed Zsh and configuration!"
