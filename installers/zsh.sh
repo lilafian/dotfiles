bc4c5391649035fd1237629f3f54e1411113da34
lilaf <lilafian74@proton.me>
2025-10-21 16:02:58 -0500

    make zsh module change default shell

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -7,3 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo " -- [zsh]: copying .zshrc to ~"
 cp -rv $REPO_DIR/dots/zsh/.zshrc ~/.zshrc
 rm ~/.zshrc.pre-oh-my-zsh
+echo " -- [zsh]: changing user default shell to zsh"
+sudo usermod --shell /bin/zsh $USER
